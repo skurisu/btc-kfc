2dbf74901d92f87b2430551c0a9dd362ed51a66b
Sarah Kurisu <sarah.kurisu@gmail.com>
2018-01-13 21:20:45 -0500

    adds favicon and detail text

--- src/App.js
+++ src/App.js
@@ -37,6 +37,9 @@ class App extends Component {
       <div className="App">
         <h1 className="main-text">BTC-KFC</h1>
         <p className="ticker">{this.state.buckets}</p>
+        <span>
+          * In USD (based on the price of KFC's 12 piece chicken only bucket)
+        </span>
         <img src={friedChickenBucket} />
       </div>
     );
