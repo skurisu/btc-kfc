00edd930c4f0ab8b0c8c304b930bd47d9b54cc94
Annie Kwok <anniek@upspringmedia.com>
2018-01-13 15:54:39 -1000

    positioned bucket, made ticker bigger

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import friedChickenBucket from './images/friedchickenbucket.svg';
-import friedChickenBucket2 from './images/friedchickenbucket_100.svg';
 
 class App extends Component {
   state = {
@@ -38,7 +37,7 @@ class App extends Component {
       <div className="App">
         <h1 className="main-text">BTC-KFC</h1>
         <img src={friedChickenBucket} />
-        <p>{this.state.buckets}</p>
+        <p className="ticker">{this.state.buckets}</p>
       </div>
     );
   }
